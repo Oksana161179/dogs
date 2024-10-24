7a2cef2b591812aab9a4354780612b87088f2b4c
Оксана <oksana.fomenko.161979@gmail.com>
2024-10-24 14:37:11 +0600

    Создана функция загрузки ссылки на изображение

--- собачки.py
+++ собачки.py
@@ -4,7 +4,15 @@ import requests #для того чтобы загружать из интерн
 from PIL import Image, ImageTk #для того чтобы обрабатывать изображения
 from io import BytesIO #для того чтобы обрабатывать картинку
 
-def get_dog_image():
+
+def get_dog_image():#создаем функцию получения изображения
+    try:#обрабатываем исключения
+        response = requests.get("https://dog.ceo/api/breeds/image/random")
+        response.raise_for_status()#узнаем статус существует ли сайт
+        data = response.json()#в дате лежит ответ от json
+        return data('message')#возвращается сообщение о сайте
+    except Exception as e:#обрабатываем исключения
+        mb.showerror("Ошибка", f"Возникла ошибка при зппросе к API {e}")
 
 def show_image():#создаем функцию которая будет выводить изображения
     image_url = get_dog_image()#получаем ссылку на изображение
@@ -17,10 +25,12 @@ def show_image():#создаем функцию которая будет выв
             # в переменную-img_data
             img = Image.open(img_data)#обрабатываем в нормальную картинку
             img.thumbnail((300, 300))#подгоняем все картинки под один размер
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)#загружаем картинку в метку
             label.image = img#чтобы сборщик мусора не собрал картинку
         except Exception as e:#обрабатываем исключения
-            mb.showerror("Ошибка", f"Возникла ошибка {e}")
+            mb.showerror("Ошибка", f"Возникла ошибка при загрузке изображения {e}")
+            return None#если возникла ошибка возвращается пустота
 
 
 
